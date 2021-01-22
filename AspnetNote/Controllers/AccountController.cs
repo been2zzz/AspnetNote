512dff9b1061a53e6d124666a2044cc6a6fc1c89
been2zzz <been2zz@naver.com>
2021-01-23 00:34:33 +0900

    Add project files.

---
+++ AspnetNote/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using AspnetNote.DataContext;
+using AspnetNote.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetNote.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// 로그인
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 회원 가입
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid) // 입력값 입력했는지 아는 함수(User required로 앎)
+            {
+                using (var db = new AspnetNoteDbContext())
+                {
+                    db.Users.Add(model); //memory에 올림
+                    db.SaveChanges(); //실제 db 저장. COMMIT!
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ AspnetNote/DataContext/AspnetNoteDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AspnetNote.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetNote.DataContext
+{
+    public class AspnetNoteDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-VV8AR2J\SQLEXPRESS;Database=AsponetNoteDb;User Id=sa;Password=qls123;");
+        }
+
+    }
+}
