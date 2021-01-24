3af0c23cd16673d85fc1e4c68bdcfd82d8b6adfd
been2zzz <been2zz@naver.com>
2021-01-25 02:53:55 +0900

    edit, remove

--- AspnetNote/Controllers/AccountController.cs
+++ AspnetNote/Controllers/AccountController.cs
@@ -38,7 +38,7 @@ namespace AspnetNote.Controllers
                         // 로그인 성공
                         //HttpContext.Session.SetInt32(key, value);
                         HttpContext.Session.SetInt32("USER_LOGIN_KEY", user.UserNo);
-                        return RedirectToAction("Index", "Home");
+                        return RedirectToAction("Index", "Note");
                     }
                 }
                 // 로그인 실패
--- AspnetNote/Controllers/NoteController.cs
+++ AspnetNote/Controllers/NoteController.cs
@@ -95,14 +95,45 @@ namespace AspnetNote.Views.Shared
                 return View(note);
             }
         }
+
+        public IActionResult EditCommit(Note model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new AspnetNoteDbContext())
+                {
+                    var note = db.Notes.FirstOrDefault(n => n.NoteNo.Equals(model.NoteNo));
+                    note.NoteTitle = model.NoteTitle;
+                    note.NoteContents = model.NoteContents;
+                    if (db.SaveChanges() > 0)
+                    {
+                        // 동일한 컨트롤러 Index로 넘기는 것
+                        return Redirect("Index");
+                    }
+                    //db.SaveChanges(); //COMMIT
+                }
+                ModelState.AddModelError(string.Empty, "저장 실패");
+            }
+            return View(model);
+        }
         
         // 게시글 삭제
-        public IActionResult Delete()
+        public IActionResult Delete(int noteNo)
         {
             if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
             {
                 // 비로그인
                 return RedirectToAction("Login", "Account");
+            }
+            using (var db = new AspnetNoteDbContext())
+            {
+                var note = db.Notes.FirstOrDefault(n => n.NoteNo.Equals(noteNo));
+                db.Notes.Remove(note);  // 삭제
+                if (db.SaveChanges() > 0)
+                {
+                    // 동일한 컨트롤러 Index로 넘기는 것
+                    return Redirect("Index");
+                }
             }
             return View();
         }
